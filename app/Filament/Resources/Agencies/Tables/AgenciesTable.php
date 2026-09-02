cffdfb3f3818fb897d39b97aaff0534e015b361a
SILAP-RD <admin@silap.com>
2026-09-02 15:26:12 +0700

    Fix: Menggunakan namespace yang benar untuk DeleteAction pada AgenciesTable dan CategoriesTable

--- app/Filament/Resources/Agencies/Tables/AgenciesTable.php
+++ app/Filament/Resources/Agencies/Tables/AgenciesTable.php
@@ -6,7 +6,7 @@ use App\Models\Agency;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
-use Filament\Tables\Actions\DeleteAction;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
